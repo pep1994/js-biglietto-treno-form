55643c24391661758c2b31bbeffed462a053ad2a
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-27 17:59:23 +0100

    script - code refactoring

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@ ageU.value = "";
 nameU.value = "";
 kmU.value = "";
 
-// dico al bottone genera di restare in "ascolto" e cosa deve accadere all'evento click
+// dico al bottone "genera" di restare in "ascolto" e cosa deve accadere all'evento click
 
 gButton.addEventListener('click',
 
   function() {
 
-    // salvo il valore inserito dall'utente in una variabile al click sul bottone genera
+    // salvo il valore inserito dall'utente in una variabile al click sul bottone "genera"
     nameValue = nameU.value;
     kmValue = parseInt(kmU.value);
     ageValue = ageU.value;
@@ -45,21 +45,24 @@ gButton.addEventListener('click',
       totPrice = price;
     }
 
-    // se il valore inserito nell'input dei km da percorrere non è un numero allora il programma lo segnalerà all'utente forzandolo a reinserire il valore
+    // se il valore inserito nell'input dei km da percorrere non è un numero allora il programma lo segnalerà all'utente forzandolo a reinserire un valore numerico
 
     if (isNaN(kmValue)) {
-      alert("Il valore dei km da percorrere non è corretto, è pregato di inserire un valore che sia un numero. Grazie!");
+
+      alert("Il valore dei km da percorrere non è corretto, è pregato di inserire un valore numerico. Grazie!");
+
       nameValue = "";
       kmValue = "";
       ageValue = "";
 
-      document.getElementById('second-container').classList.remove('visible');
       document.getElementById('km').value = kmValue;
 
+      // altrimenti proseguirà normalmente
+
     } else {
+
       // creo un numero casuale da 1 a 10 che corrisponderà al numero della cabina
       nCab = Math.floor(Math.random() * 10 + 1);
-
 
       // creo un numero casuale che corrisponderà al codice cp della cabina
       codeCab = Math.round(Math.random() * 99000 + 1000);
@@ -91,21 +94,22 @@ gButton.addEventListener('click',
   });
 
 
-// dico al bottone annulla di restare in "ascolto" e cosa deve accadere all'evento click
+// dico al bottone "annulla" di restare in "ascolto" e cosa deve accadere all'evento click
 
 aButton.addEventListener('click',
 
   function() {
 
-    // al momento del click sul bottone annulla scompare il secondo blocco
+    // al momento del click sul bottone "annulla" scompare il secondo blocco
 
     document.getElementById('second-container').classList.remove('visible');
 
-    // al momento del click sul bottone annulla  si resettano tutti valori che l'utente ha inserito negli input
+    // al momento del click sul bottone "annulla" si resettano tutti valori che l'utente ha inserito negli input
 
     nameValue = "";
     kmValue = "";
     ageValue = "";
+
     document.getElementById('name').value = nameValue;
     document.getElementById('km').value = kmValue;
     document.getElementById('age').value = ageValue;
