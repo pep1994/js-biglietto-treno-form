edb69b76c852b38eb1a33441bb9573128bc8a38b
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-27 17:21:26 +0100

    script - reset e scomparsa secondo blocco al click del bottone annulla

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ L'utente inserisce negli input il proprio nome, i km che deve percorrere e la fa
 */
 
 // dichiarazione variabili
-var nameU, kmU, ageU, gButton, nameValue, kmValue, ageValue, price, totPrice, nCab, codeCab;
+var nameU, kmU, ageU, gButton, aButton, nameValue, kmValue, ageValue, price, totPrice, nCab, codeCab;
 var basePrice = 0.21; // variabile che rappresenta il prezzo al kilometro
 var scontoMinori = 20 / 100; // variabile che rappresenta lo sconto per i minorenni
 var scontoOver65 = 40 / 100; // variabile che rappresenta lo sconto per gli Over65
@@ -15,7 +15,10 @@ nameU = document.getElementById('name');
 kmU = document.getElementById('km');
 ageU = document.getElementById('age');
 gButton = document.getElementById('genera');
+aButton = document.getElementById('annulla');
 
+// imposto di default il valore vuoto per l'input della fascia d'età
+ageU.value = "";
 
 // dico al bottone genera di restare in "ascolto" e cosa deve accadere all'evento click
 
@@ -27,9 +30,9 @@ gButton.addEventListener('click',
     nameValue = nameU.value;
     kmValue = parseInt(kmU.value);
     ageValue = ageU.value;
-    console.log(nameValue, kmValue, ageValue);
 
     // calcolo il prezzo del biglietto in base alla fascia d'età dell'utente
+
     price = basePrice * kmValue;
 
     if (ageValue === "Under 18") {
@@ -40,15 +43,14 @@ gButton.addEventListener('click',
       totPrice = price;
     }
 
-    console.log(totPrice);
 
     // creo un numero casuale da 1 a 10 che corrisponderà al numero della cabina
     nCab = Math.floor(Math.random() * 10 + 1);
-    console.log(nCab);
+
 
     // creo un numero casuale che corrisponderà al codice cp della cabina
     codeCab = Math.round(Math.random() * 99000 + 1000);
-    console.log(codeCab);
+
 
     // output nome utente sul biglietto
     document.getElementById('userName').innerHTML = nameValue;
@@ -71,5 +73,26 @@ gButton.addEventListener('click',
     // aggiunta di una classe al secondo blocco in modo da farlo comparire al click sul bottone
     document.getElementById('second-container').className = "visible"
 
+  });
+
+
+// dico al bottone annulla di restare in "ascolto" e cosa deve accadere all'evento click
+
+aButton.addEventListener('click',
+
+  function() {
+
+    // al momento del click sul bottone annulla scompare il secondo blocco
+
+    document.getElementById('second-container').classList.remove('visible');
+
+    // al momento del click sul bottone annulla  si resettano tutti valori che l'utente ha inserito negli input
+
+    nameValue = "";
+    kmValue = "";
+    ageValue = "";
+    document.getElementById('name').value = nameValue;
+    document.getElementById('km').value = kmValue;
+    document.getElementById('age').value = ageValue;
 
   });
